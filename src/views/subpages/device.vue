61103ff017e780e95ea4db8658b7f7dd3489fb72
gayashan <gayashan.randimagamage@gmail.com>
2025-06-23 16:24:00 +0530

    add several sub pages

--- src/views/subpages/device.vue
+++ src/views/subpages/device.vue
@@ -1,8 +1,31 @@
 <template>
     <div class="flex flex-col gap-10">
         <h1 class="font-semibold text-2xl">Device management</h1>
+        <!-- gate sensors -->
         <div class="flex flex-col gap-4 rounded-sm">
-            <h3 class="font-semibold text-lg text-gray-400">Upcomming bookings</h3>
+            <h3 class="font-semibold text-lg text-gray-400">Gate sensors</h3>
+            <table class="w-full border-collapse">
+                <thead class="sm:table-header-group hidden bg-gray-100">
+                    <tr class="text-left">
+                        <th class="min-w-[130px] w-[200px] pl-6 py-2 text-gray-400 uppercase text-sm">Gate</th>
+                        <th class="min-w-[130px] w-[200px] pl-6 py-2 text-gray-400 uppercase text-sm">Sensor ID</th>
+                        <th class="min-w-[130px] w-[200px] pl-6 py-2 text-gray-400 uppercase text-sm">Status</th>
+                        <th class="min-w-[130px] w-[200px] pl-6 py-2 text-gray-400 uppercase text-sm">Bettery status</th>
+                    </tr>
+                </thead>
+                <tbody class="sm:table-row-group flex flex-col gap-4" >
+                    <tr class="sm:table-row flex flex-col sm:border-1 sm:border-b sm:border-gray-200 border-2 border-gray-200 sm:odd:bg-gray-50 sm:even:bg-white bg-gray-50 hover:bg-gray-100 sm:p-0 p-4 sm:rounded-none rounded-md" v-for="item in gateSensors">
+                        <td class="sm:table-cell flex flex-row py-2 pl-6 font-bold text-gray-400 uppercase text-sm">{{ item.sensorName.slice(0,3) == 'opn' ? 'Entry' : 'Exite' }}</td>
+                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.sensorName }}</td>
+                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.status == true ? 'Ok' : 'Offline' }}</td>
+                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.bettery }}<span class="text-xs"> %</span></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!-- slot sensors -->
+        <div class="flex flex-col gap-4 rounded-sm">
+            <h3 class="font-semibold text-lg text-gray-400">Slot sensors</h3>
             <table class="w-full border-collapse">
                 <thead class="sm:table-header-group hidden bg-gray-100">
                     <tr class="text-left">
@@ -13,11 +36,11 @@
                     </tr>
                 </thead>
                 <tbody class="sm:table-row-group flex flex-col gap-4" >
-                    <tr class="sm:table-row flex flex-col sm:border-1 sm:border-b sm:border-gray-200 border-2 border-gray-200 sm:odd:bg-gray-50 sm:even:bg-white bg-gray-50 hover:bg-gray-100 sm:p-0 p-4 sm:rounded-none rounded-md" v-for="item in upcomming">
-                        <td class="sm:table-cell flex flex-row py-2 pl-6 font-bold text-gray-400 uppercase text-sm">{{ item.vahicleId }}</td>
-                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.enter }}</td>
-                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.leave }}</td>
-                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.slot }}</td>
+                    <tr class="sm:table-row flex flex-col sm:border-1 sm:border-b sm:border-gray-200 border-2 border-gray-200 sm:odd:bg-gray-50 sm:even:bg-white bg-gray-50 hover:bg-gray-100 sm:p-0 p-4 sm:rounded-none rounded-md" v-for="item in slotSensors">
+                        <td class="sm:table-cell flex flex-row py-2 pl-6 font-bold text-gray-400 uppercase text-sm">{{ item.sensorName.slice(0,2) }}</td>
+                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.sensorName }}</td>
+                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.status == true ? 'Ok' : 'Offline' }}</td>
+                        <td class="sm:table-cell flex flex-row py-2 pl-6">{{ item.bettery }}<span class="text-xs"> %</span></td>
                     </tr>
                 </tbody>
             </table>
@@ -25,4 +48,135 @@
     </div> 
 </template>
 <script setup>
+import { onMounted, ref } from 'vue';
+
+const gateSensors = ref([])
+const slotSensors = ref([])
+
+const sensorData = ref([
+    {
+        'sensorName' : 'E1U',
+        'status' : true,
+        'bettery' : 27
+    },
+    {
+        'sensorName' : 'E1M',
+        'status' : true,
+        'bettery' : 17
+    },
+    {
+        'sensorName' : 'E2U',
+        'status' : true,
+        'bettery' : 69
+    },
+    {
+        'sensorName' : 'E2M',
+        'status' : false,
+        'bettery' : 27
+    },
+    {
+        'sensorName' : 'D1U',
+        'status' : true,
+        'bettery' : 45
+    },
+    {
+        'sensorName' : 'D1M',
+        'status' : false,
+        'bettery' : 44
+    },
+    {
+        'sensorName' : 'D2U',
+        'status' : true,
+        'bettery' : 27
+    },
+    {
+        'sensorName' : 'D2M',
+        'status' : true,
+        'bettery' : 57
+    },
+    {
+        'sensorName' : 'P1U',
+        'status' : false,
+        'bettery' : 27
+    },
+    {
+        'sensorName' : 'P1M',
+        'status' : true,
+        'bettery' : 27
+    },
+    {
+        'sensorName' : 'P2U',
+        'status' : false,
+        'bettery' : 27
+    },
+    {
+        'sensorName' : 'P2M',
+        'status' : true,
+        'bettery' : 67
+    },
+    {
+        'sensorName' : 'A1U',
+        'status' : true,
+        'bettery' : 8
+    },
+    {
+        'sensorName' : 'A1M',
+        'status' : true,
+        'bettery' : 20
+    },
+    {
+        'sensorName' : 'A2U',
+        'status' : true,
+        'bettery' : 47
+    },
+    {
+        'sensorName' : 'A2M',
+        'status' : true,
+        'bettery' : 57
+    },
+    {
+        'sensorName' : 'A3U',
+        'status' : true,
+        'bettery' : 27
+    },
+    {
+        'sensorName' : 'A3M',
+        'status' : true,
+        'bettery' : 27
+    },
+    // this is for open gate
+    {
+        'sensorName' : 'opnIL',
+        'status' : true,
+        'bettery' : 40
+    },
+    {
+        'sensorName' : 'opnLDR',
+        'status' : false,
+        'bettery' : 87
+    },
+    // this is for close gate
+    {
+        'sensorName' : 'clsLDR',
+        'status' : true,
+        'bettery' : 47
+    },
+    {
+        'sensorName' : 'clsIL',
+        'status' : true,
+        'bettery' : 17
+    },
+])
+
+// devide sensor data base on usege (gate / slots)
+onMounted(() => {
+    for (let i = 0; i < sensorData.value.length; i++) {
+        console.log(sensorData.value[i].sensorName.slice(0,3))
+        if (sensorData.value[i].sensorName.slice(0, 3) == 'opn' || sensorData.value[i].sensorName.slice(0, 3) == 'cls') {
+            gateSensors.value.push(sensorData.value[i])
+        }else{
+            slotSensors.value.push(sensorData.value[i])
+        }
+    }
+})
 </script>
--- src/views/subpages/eventProcessing.vue
+++ src/views/subpages/eventProcessing.vue
@@ -1,7 +1,75 @@
 <template>
     <div class="flex flex-col gap-10">
         <h1 class="font-semibold text-2xl">Event Processing</h1>
+        <div class="flex flex-col gap-4 rounded-sm">
+            <table class="w-full border-collapse">
+                <thead class="sm:table-header-group hidden bg-gray-100">
+                    <tr class="text-left">
+                        <th class="min-w-[100px] w-[100px] pl-6 py-2 text-gray-400 uppercase text-sm">Time</th>
+                        <th class="min-w-[100px] w-[80px] pl-6 py-2 text-gray-400 uppercase text-sm">Tage</th>
+                        <th class="min-w-[130px] w-[500px] pl-6 py-2 text-gray-400 uppercase text-sm">Description</th>
+                    </tr>
+                </thead>
+                <tbody class="sm:table-row-group flex flex-col gap-4" >
+                    <tr class="sm:table-row flex flex-col sm:border-1 sm:border-b sm:border-gray-200 border-2 border-gray-200 sm:odd:bg-gray-50 sm:even:bg-white bg-gray-50 hover:bg-gray-100 sm:p-0 p-4 sm:rounded-none rounded-md h-14" v-for="(item, index) in notification">
+                        <td class="sm:table-cell flex flex-row py-2 pl-6 font-bold text-gray-400 uppercase text-sm">{{ new Date(item.time).toLocaleString().slice(0,-2) }}</td>
+                        <td class="sm:table-cell flex flex-row py-2 pl-6"><p :class="{'border-2 border-blue-500 text-blue-500' : item.tag == 'booking', 'border-2 border-green-600 text-green-500' : item.tag == 'vehicle', 'bg-red-400' : item.tag == 'sensor',}" class="w-fit py-1 px-2 rounded-full text-xs"><i class="fi fi-rr-exclamation mr-2" v-if="item.tag == 'sensor'"></i>{{ item.tag }}</p></td>
+                        <td class="sm:table-cell flex flex-row py-2 pl-6 max-w-[100px] cursor-pointer" @click="viewAllText(item.message, index)" :id="'explanation' + index">{{ item.message.slice(0, 110) }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div> 
 </template>
 <script setup>
-</script>z
+import { ref } from 'vue';
+
+const notification = ref([
+    {
+        time : '2025-06-23T09:37:57.757Z',
+        tag : 'sensor',
+        message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptas aliquid animi voluptates ducimus voluptatem quaerat similique est, odit delectus nulla distinctio odio rem consectetur necessitatibus nostrum praesentium velit sapiente.'
+    },
+    {
+        time : '2025-06-23T09:37:57.757Z',
+        tag : 'booking',
+        message : 'iquid animi voluptates ducimus voluptatem quaerat similique est, odit delectus nulla distinctio odio rem consectetur necessitatibus nostrum praesentium velit sapiente.'
+    },
+    {
+        time : '2025-06-23T09:37:57.757Z',
+        tag : 'sensor',
+        message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.  quaerat similique est, odit delectus nulla distinctio odio rem consectetur necessitatibus nostrum praesentium velit sapiente.'
+    },
+    {
+        time : '2025-06-23T09:37:57.757Z',
+        tag : 'sensor',
+        message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptas aliquid animi voluptates ducimus voluptatem quaerat similique est, odit delectus nulla distinctio odio rem consectetur necessitatibus nostrum praesentium velit sapiente.'
+    },
+    {
+        time : '2025-06-23T09:37:57.757Z',
+        tag : 'booking',
+        message : 'Lorem ipsum, dolor uaerat similique est, odit delectus nulla distinctio odio rem consectetur necessitatibus nostrum praesentium velit sapiente.'
+    },
+    {
+        time : '2025-06-23T09:37:57.757Z',
+        tag : 'vehicle',
+        message : 'Lorem ipsum, dolor uaerat similique est, odit delectus nulla distinctio odio rem consectetur necessitatibus nostrum praesentium velit sapiente.'
+    },
+    {
+        time : '2025-06-23T09:37:57.757Z',
+        tag : 'vehicle',
+        message : 'Lorem ipsum, dolor uaerat similique est, odit delectus nulla distinctio odio rem consectetur necessitatibus nostrum praesentium velit sapiente.'
+    },
+])
+
+const viewAllText = (msg, index) => {
+    const element = document.querySelector(`#explanation${index}`)
+    const paragraph = element.textContent
+    if(paragraph.length < 120){
+        element.textContent = msg
+    }else{
+        element.textContent = msg.slice(0, 110)
+    }
+}
+
+</script>
